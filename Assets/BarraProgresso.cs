883a0e1b9aca8e5c9ac7c431a72b11ca40d2bd54
calvinfranca <calvin.franca@gmail.com>
2020-06-29 03:21:24 -0300

    cooldown

---
+++ Assets/BarraProgresso.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BarraProgresso : MonoBehaviour
+{
+    public float max;
+    public float atual=11;
+    public float min=0;
+    
+    public Image fill;
+    
+    public Controle control;
+    public float timer=1; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        GetCurrentFill();
+    }
+    void GetCurrentFill()
+    {
+        max = control.cooldown;
+        
+        atual = control.cooldown - control.tempo;
+
+      
+        
+        float fillAmount = (float)atual /(float)max;
+        fill.fillAmount = fillAmount;
+
+        
+    }
+}
--- Assets/Controle.cs
+++ Assets/Controle.cs
@@ -8,17 +8,23 @@ public class Controle : MonoBehaviour
 
         
 
-   Vector3 playeraxis;
-   public CharacterController cctrl;
-   public GameObject projetil;
-   public GameObject especial_dispersao;
+    Vector3 playeraxis;
+    public CharacterController cctrl;
+    public GameObject projetil;
+    public GameObject especial_dispersao;
+    public float cooldown=10;
+    public float nextFireTime=0;
+    public float tempo = 10;
+    
 
-   public Camera cam;
-   public GameObject foco;
-   
-   Vector2 mousePos;
-    //public GameObject head;
-    // Start is called before the first frame update
+
+
+
+    public Camera cam;
+    public GameObject foco;
+    
+    Vector2 mousePos;
+     
     public float bombForce = 1000;
     Quaternion newq = new Quaternion(0, 90, 0, 1);
     Vector3 vec = new Vector3(0, 90, 0);
@@ -30,6 +36,12 @@ public class Controle : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
+        if (tempo <= 10)
+        {
+            tempo -= Time.deltaTime;
+        }
+        
+        
         if (Time.timeScale < 1)
             return;
        playeraxis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -46,13 +58,19 @@ public class Controle : MonoBehaviour
             
          
         }
-        if (Input.GetButtonDown("Fire2"))
+        if (Time.time > nextFireTime)
         {
-            GameObject currentespecial = Instantiate(especial_dispersao, transform.position + transform.forward, especial_dispersao.transform.rotation);
-            currentespecial.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
-            
-        }
+            if (Input.GetButtonDown("Fire2"))
+            {
+                GameObject currentespecial = Instantiate(especial_dispersao, transform.position + transform.forward, especial_dispersao.transform.rotation);
+                currentespecial.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
 
+                nextFireTime = Time.time + cooldown;
+                tempo = 10f;
+                
+
+            }
+        }
 
     }
     
