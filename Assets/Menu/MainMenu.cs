808c836724fb667c98458fbedc2c683a502fde11
calvinfranca <calvin.franca@gmail.com>
2020-06-29 01:06:19 -0300

    inicio

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
